a6474d9fc393bbe36bd117dbe01d7b48e9bf7678
Ana Reyna <anareymej@gmail.com>
2017-10-19 20:21:03 -0500

    wip(site): dummy

--- source/app/pages/index.vue
+++ source/app/pages/index.vue
@@ -8,69 +8,44 @@
       )
     br
     br
-    - for (var i = 0; i < 2; i++)
-      nuxt-link.job-item(to="/detalle")
-        .job-item__company-logo
-          img(src="https://getonbrd-prod.s3.amazonaws.com/uploads/users/logo/2165/thumb_tw_avatar-01.png")
-        h3.job-item__title  Javascript Developer
-        span.job-item__company Empresa
-        | &nbsp; • &nbsp;
-        span.job-item__type.job-item__type--full Full-time
-        | &nbsp; • &nbsp;
-        span.job-item__location Lima
+    nuxt-link.job-item(to="/detalle" v-for="job in jobs" :key="job.id")
+      .job-item__company-logo
+        img(:src="job.company_logo")
+      h3.job-item__title  {{ job.position_name }}
+      span.job-item__company {{ job.company_name }}
+      | &nbsp; • &nbsp;
+      span.job-item__type.job-item__type--full {{ job.job_type }}
+      | &nbsp; • &nbsp;
+      span.job-item__location {{ job.location }}
 
-      nuxt-link.job-item(to="/detalle")
-        .job-item__company-logo
-          img(src="https://vuejobs.com/storage/avatars/tXrNI83Ji4vLBKkyCg5oDFUNCy5caX9TN22q3cN1.png")
-        h3.job-item__title  Desarrollador (Back End/DevOps)
-        span.job-item__company Empresa
-        | &nbsp; • &nbsp;
-        span.job-item__type.job-item__type--part Part-time
-        | &nbsp; • &nbsp;
-        span.job-item__location Lima
-
-      nuxt-link.job-item(to="/detalle")
-        .job-item__company-logo
-          img(src="https://getonbrd-prod.s3.amazonaws.com/uploads/users/logo/4976/thumb_logo-bbva-continental-square.png")
-        h3.job-item__title  Front-end developer
-        span.job-item__company Empresa
-        | &nbsp; • &nbsp;
-        span.job-item__type.job-item__type--contract Contrato
-        | &nbsp; • &nbsp;
-        span.job-item__location Lima
-
-      nuxt-link.job-item(to="/detalle")
-        h3.job-item__title  Front-end developer junior
-        span.job-item__company Empresa
-        | &nbsp; • &nbsp;
-        span.job-item__type.job-item__type--contract Contrato
-        | &nbsp; • &nbsp;
-        span.job-item__location Lima
-    br
-    el-pagination(
+    el-pagination.u-mt20(
         layout="prev, pager, next"
         :total="50"
         )
 
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  },
-  data () {
-    return {
-      txtSearch: null
-    }
-  }
-}
-</script>
-
 <style lang="stylus" scoped>
 .el-pagination
   text-align right
 
 </style>
+
+<script>
+import axios from 'axios'
+
+export default {
+
+  async asyncData () {
+    const { data } = await axios.get('https://api.mockaroo.com/api/72033920?count=30&key=b3c09a30')
+    return { jobs: data }
+  },
+  data () {
+    return {
+      txtSearch: null,
+      jobs: null
+    }
+  }
+}
+</script>
+
